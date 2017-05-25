16db435eb2e7003a7c611fafbf2786a92764f4e0
John Vandenberg <jayvdb@gmail.com>
2017-05-25 18:23:48 +0700

    Preinstall atdgen=1.10.0

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -154,9 +154,9 @@ if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
   wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
   tar xf ~/infer.tar.xz -C ~/
   cd ~/infer-linux64-v0.7.0
-  sed -i 's/"atdgen" {>="1.10.0"}/"atdgen" {="1.10.0"}/' opam
   opam init --y
   opam update
+  opam install "atdgen=1.10.0"
   opam pin add --yes --no-action infer .
   opam install --deps-only --yes infer
   ./build-infer.sh java

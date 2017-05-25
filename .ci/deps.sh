303ac30d39be75c49a5914ce7e938cabb9f128a9
John Vandenberg <jayvdb@gmail.com>
2017-05-25 16:40:42 +0700

    Pin atdgen=1.10.0

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -153,6 +153,7 @@ if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
   wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
   tar xf ~/infer.tar.xz -C ~/
   cd ~/infer-linux64-v0.7.0
+  sed -i 's/"atdgen" {>="1.10.0"}/"atdgen" {="1.10.0"}/' opam
   opam init --y
   opam update
   opam pin add --yes --no-action infer .
